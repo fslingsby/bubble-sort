1160027a4e6e341e4eaf4ed0f4bc0e386772420e
fslingsby <f.t.slingsby@gmail.com>
2025-02-01 16:05:31 +0000

    Add files via upload

--- bubblesort/main.py
+++ bubblesort/main.py
@@ -2,17 +2,24 @@ number_list = []
 num1 = input("Enter the first number to be sorted: ")
 number_list.append(int(num1))
 adding = True
+num2 = input("Add more numbers to the list, pressing enter each time, "
+                 "and when you are ready to begin the sort, type 'sort': ")
+number_list.append(int(num2))
 while adding:
-    next_num = input("Add another number to the list, or type 'sort' to sort the list in ascending order: ")
+    next_num = input()
     if next_num == 'sort':
         adding = False
     else:
         number_list.append(int(next_num))
+order = input("What order would you like the list? Choose 'a' for ascending, or 'd' for descending: ")
 
 sorted_list = [number_list[0]]
 
 for i in number_list[1:]:
-    print(sorted_list)
+    if order == 'd':
+        print(sorted_list[::-1])
+    else:
+        print(sorted_list)
     pos = 0
     bigger = True
     while bigger:
